8a103962ab250a91fc81a135287f01aa66933bbe
IudexErise <vyacheslav.saprykin@gmail.com>
2022-05-14 13:49:27 +0300

    1

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 let images = [{
     url: "./images/slider-image_one.png",
-    location: "Rostov-on-Don LCD admiral",
+    location: "Rostov-on-Don <br> LCD admiral",
     area: "81 m2",
     time: "3.5 months",
+    link: "Rostov-on-Don, Admiral",
 }, {
-    url: "/images/slider-image_two.png",
-    location: "Sochi Thieves",
+    url: "./images/slider-image_two.png",
+    location: "Sochi <br> Thieves",
     area: "105 m2",
     time: "4 months",
+    link: "Sochi Thieves",
 }, {
     url: "./images/slider-image_three.png",
-    location: "Rostov-on-Don Patriotic",
+    location: "Rostov-on-Don <br> Patriotic",
     area: "93 m2",
     time: "3 months",
+    link: "Rostov-on-Don Patriotic",
 }];
 
 function initSlider() {
   if(!images || !images.length) return;
     
-    let sliderImages = document.querySelector(".slider__images");
-    let sliderArrows = document.querySelector(".slider__arrows");
-    let sliderDots = document.querySelector(".slider__dots");
+    let sliderImages = document.querySelector(".slider_images");
+    let sliderArrows = document.querySelector(".slider_arrows");
+    let sliderDots = document.querySelector(".slider_dots");
+    let sliderLinks = document.querySelector(".slider_links");
     
     initImages();
     initArrows();
     initDots(); 
-    initDetails();     
+    initDetails();  
+    initLinks();  
   
     function initImages() {
       images.forEach((image, index) => {
@@ -35,7 +40,7 @@ function initSlider() {
     }
     
     function initArrows() {
-      sliderArrows.querySelectorAll(".slider__arrow").forEach(arrow => {
+      sliderArrows.querySelectorAll(".slider_arrow").forEach(arrow => {
         arrow.addEventListener("click", function() {
           let curNumber = +sliderImages.querySelector(".active").dataset.index;
           let nextNumber;
@@ -51,7 +56,7 @@ function initSlider() {
     
     function initDots() {
       images.forEach((image, index) => {
-        let dot = `<div class="slider__dots-item n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
+        let dot = `<div class="slider_dots-item n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
         sliderDots.innerHTML += dot;
       });
       sliderDots.querySelectorAll(".slider__dots-item").forEach(dot => {
@@ -72,12 +77,26 @@ function initSlider() {
           if (timeNode) timeNode.innerHTML = img.time;
       }
     }
+
+    function initLinks() {
+      images.forEach((image, index) => {
+          let linkDiv = `<div class="slider_links-item n${index} ${index === 0 ? "active" : ""}" data-index="${index}">${image.title}</div>`;
+          sliderLinks.innerHTML += linkDiv;
+      });
+      sliderLinks.querySelectorAll(".slider_links-item").forEach(links => {
+          links.addEventListener("click", function() {
+              moveSlider(this.dataset.index); 
+          });
+      });
+  }
     
     function moveSlider(num) {
       sliderImages.querySelector(".active").classList.remove("active");
       sliderImages.querySelector(".n" + num).classList.add("active");
       sliderDots.querySelector(".active").classList.remove("active");
       sliderDots.querySelector(".n" + num).classList.add("active");
+      sliderLinks.querySelector(".active").classList.remove("active");
+      sliderLinks.querySelector(".n" + num).classList.add("active");
       initDetails(num);
     }    
 
